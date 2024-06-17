d96225ba6f15641224e552131e919598a5fabb90
Saahil Sharma <saahilsharma1017@gmail.com>
2024-06-17 18:14:25 -0400

    ui complete + logic complete

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,7 +1,16 @@
-export default function todoInput() {
+import { useState } from "react"
+
+export default function TodoInput(props) {
+    const { handleAddTodos, todoValue, setTodoValue } = props
     return (
-    <header>
-        TodoInput
-    </header>
-    );
+        <header>
+            <input value={todoValue} onChange={(e) => {
+                setTodoValue(e.target.value)
+            }} placeholder="Enter todo..." />
+            <button onClick={() => {
+                handleAddTodos(todoValue)
+                setTodoValue('')
+            }}>Add</button>
+        </header>
+    )
 }
